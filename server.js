3632bacb84c178e204de1b8a576105067655e6cc
sara heymann <sara.b.heymann@gmail.com>
2018-08-07 13:10:57 -0500

    changed the routes back

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
-var routes = require("./Routes/htmlRoutes");
-
 
 var app = express();
 
@@ -12,7 +10,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-app.use(routes);
 
 // app.use(express.static('/Public'))
 // app.use(express.static(process.cwd() + '/Public'));
@@ -21,6 +18,8 @@ app.use(routes);
 
 // app.use(express.static(dir));
 app.use(express.static(__dirname + '/public'));
+
+require("./Routes/htmlRoutes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
