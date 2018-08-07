13eea894b56793841f0748a8881aab5aea43dbba
sara heymann <sara.b.heymann@gmail.com>
2018-08-07 14:22:30 -0500

    deleting public from art.html and routes

--- Routes/htmlRoutes.js
+++ Routes/htmlRoutes.js
@@ -3,20 +3,23 @@ var path = require("path");
 module.exports = function(app) {
     
     app.get("/art", function(req, res) {
-      res.sendFile(path.join(__dirname, "./public/art.html"));
+      res.sendFile(path.join(__dirname, "/art.html"));
     });
   
     app.get("/contact", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/contact.html"));
+      res.sendFile(path.join(__dirname, "/contact.html"));
     });
 
     app.get("/news", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/news.html"));
+      res.sendFile(path.join(__dirname, "/news.html"));
     });
 
     app.get("/web", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/web.html"));
+      res.sendFile(path.join(__dirname, "/web.html"));
     });
+    // app.get("/web", function(req, res) {
+    //   res.sendFile(path.join(__dirname, "../public/web.html"));
+    // });
   
     // If no matching route is found default to home
     app.get("*", function(req, res) {
