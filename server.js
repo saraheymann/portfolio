7dd895f5878d9ab76e74df025fabaa3c231acf59
sara heymann <sara.b.heymann@gmail.com>
2018-08-13 12:17:27 -0500

    changed the uppercase logo, deleted some unused code

--- server.js
+++ server.js
@@ -12,8 +12,6 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // app.use(express.static('/Public'))
-// app.use(express.static(process.cwd() + '/Public'));
-// app.use(express.static(__dirname + '/Public'));
 // var dir = path.join(__dirname, 'public');
 
 // app.use(express.static(dir));
