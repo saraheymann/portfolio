2def0efe788b8757454a6ae51691f5c0089c4477
sara heymann <sara.b.heymann@gmail.com>
2018-08-14 12:43:35 -0500

    reverting back

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var path = require("path");
 var app = express();
 
 var PORT = process.env.PORT || 3000;
+app.use(express.static(process.cwd() + '/public'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,7 +16,6 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 // var dir = path.join(__dirname, 'public');
 // app.use(express.static(dir));
 // app.use(express.static(__dirname + '/public'));
-app.use(express.static(process.cwd() + '/public'));
 // process.env.PWD = process.cwd();
 // app.use(express.static(path.join(process.env.PWD, 'public')));
 // app.use(express.static("public"));
