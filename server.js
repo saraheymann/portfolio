cb4ac2d6e533aeba493ccb5e0a5054c8cdac8b84
sara heymann <sara.b.heymann@gmail.com>
2018-08-20 16:46:51 -0500

    trying out dotenv

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 // var nodemailer = require(nodemailer);
-// require('dotenv').config();
+require('dotenv').config();
 
 var app = express();
 
