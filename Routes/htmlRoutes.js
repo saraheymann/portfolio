a5e7766e4d3e707cc23e8caa89f7259e4190c714
sara heymann <sara.b.heymann@gmail.com>
2018-08-20 16:00:13 -0500

    app error blocking out nodemailer

--- Routes/htmlRoutes.js
+++ Routes/htmlRoutes.js
@@ -19,31 +19,31 @@ module.exports = function(app) {
       res.sendFile(path.join(__dirname, "../Public/web.html"));
     });
 
-    app.post('/contact', function (req, res) {
-      let mailOpts, smtpTrans;
-      smtpTrans = nodemailer.createTransport({
-        host: 'mail.saraheymann.com',
-        port: 465,
-        secure: true,
-        auth: {
-          user: MAIL_USER,
-          pass: MAIL_PASS
-        }
-      });
-      mailOpts = {
-        from: req.body.name + ' &lt;' + req.body.email + '&gt;',
-        to: GMAIL_USER,
-        subject: 'New message from contact form at tylerkrys.ca',
-        text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`
-      };
-      smtpTrans.sendMail(mailOpts, function (error, response) {
-        if (error) {
-          res.render('contact-failure');
-        }
-        else {
-          res.render('contact-success');
-        }
-      });
+    // app.post('/contact', function (req, res) {
+    //   let mailOpts, smtpTrans;
+    //   smtpTrans = nodemailer.createTransport({
+    //     host: 'mail.saraheymann.com',
+    //     port: 465,
+    //     secure: true,
+    //     auth: {
+    //       user: MAIL_USER,
+    //       pass: MAIL_PASS
+    //     }
+    //   });
+    //   mailOpts = {
+    //     from: req.body.name + ' &lt;' + req.body.email + '&gt;',
+    //     to: GMAIL_USER,
+    //     subject: 'New message from contact form at tylerkrys.ca',
+    //     text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`
+    //   };
+    //   smtpTrans.sendMail(mailOpts, function (error, response) {
+    //     if (error) {
+    //       res.render('contact-failure');
+    //     }
+    //     else {
+    //       res.render('contact-success');
+    //     }
+    //   });
     });
   
     // If no matching route is found default to home
